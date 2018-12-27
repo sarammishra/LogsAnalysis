c800f2349aacfb588afb7732e0ba6fecfaf69e26
SMishra <36138525+sarammishra@users.noreply.github.com>
2018-12-27 08:54:05 -0600

    Update main.py

--- main.py
+++ main.py
@@ -11,31 +11,29 @@ articlesQuery = ''' select articles.title as Article,
              group by articles.title order by
              hits DESC limit 3; '''
 
-#Place the articles query within a function whichc will later be called
+# Place the articles query within a function whichc will later be called
+
+
 def print_query1(query):
     results = get_query(query)
     print('\nQ1.The 3 most popular articles:\n')
     for i in results:
         print ('\t' + str(i[0]) + ' - ' + str(i[1]) + ' hits')
-
-
-
-# 2. Who are the most popular authors of all time?
+# Who are the most popular authors of all time?
 authorsQuery = ''' select authors.name as Author, count(*) as Views
              from log,articles,authors
              where log.path=CONCAT('/article/',articles.slug)
              AND articles.author = authors.id
              Group By authors.name
              order by views DESC '''
+# Place the articles query within a function whichc will later be called
 
-##Place the articles query within a function whichc will later be called
+
 def print_query2(query):
     results = get_query(query)
     print('\nQ2.Most Popular Authors:\n')
     for j in results:
         print ('\t' + str(j[0]) + ' - ' + str(j[1]) + ' views')
-
-
 # 3. On which days did more than 1% of requests lead to errors?
 errorQuery = ''' select date, (fail * 1.0/ total) * 100 FailurePercentage
                 FROM (SELECT cast(time as date) date,
@@ -43,24 +41,25 @@ errorQuery = ''' select date, (fail * 1.0/ total) * 100 FailurePercentage
                 then 1 else 0 END) as fail
                 FROM log group by date) as notfail
                 where ((fail * 1.0/ total) * 1.0) * 100 > 1 '''
+# Place the articles query within a function whichc will later be called
 
-##Place the articles query within a function whichc will later be called
+
 def print_query3(query):
     results = get_query(query)
     print('\nQ3.error days:\n')
     for k in results:
         print ('\t' + str(k[0]) + ' - ' + str(k[1]) + ' %')
+# establish connection to makeshift psql instance
 
-#establish connection to makeshift psql instance
+
 def get_query(query):
     '''Connect to news db'''
     db = psycopg2.connect("dbname=news")
     c = db.cursor()
     c.execute(query)
     return c.fetchall()
-
-
 # print out results
+
 print_query1(articlesQuery)
 print_query2(authorsQuery)
 print_query3(errorQuery)
